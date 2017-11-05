3601f4b4f484393de6521caf54ce28a85d3bd1d1
lbernardi <lucas.bernardi@booking.com>
2017-11-05 23:03:22 +0100

    fixed file names for criteo data

--- analyse_criteo.sh
+++ analyse_criteo.sh
@@ -1,5 +1,5 @@
 set -x
 bash prepare_criteo.sh
 bash cache.sh tmp/criteo_train.tsv.gz tmp/criteo_test.tsv.gz 0
-bash scan_hash_space.sh criteo_log.csv
-bash count_collisions.sh tmp/criteo_train.tsv.gz 0 criteo_log.csv
+bash scan_hash_space.sh criteo_hash_log.csv
+bash count_collisions.sh tmp/criteo_train.tsv.gz 0 criteo_col_log.csv
--- count_collisions.sh
+++ count_collisions.sh
@@ -6,7 +6,7 @@ do
         zcat tmp/min_train.vw.gz | vw -b $i --invert_hash tmp/hash.csv --initial_weight 100 2>/dev/null;
         echo $i $(tail -n+12 tmp/hash.csv | wc -l) $(tail -n+12 tmp/hash.csv | awk -F: '{print $2}' | sort | uniq | wc -l) >> $3
 done
-for i in {28..40}
+for i in {28..32}
 do
 	rm tmp/hash.csv
 	zcat tmp/min_train.vw.gz | vw -b $i --sparse_weights --invert_hash tmp/hash.csv --initial_weight 100 2>/dev/null;
