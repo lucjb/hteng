5e1159feb9067459d4a7d7778f76cc1a6540332a
lbernardi <lucas.bernardi@booking.com>
2017-11-05 18:46:56 +0100

    fixed count collisions bug, added output files

--- count_collisions.sh
+++ count_collisions.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 zcat $1 | pypy minfeatureset.py | pypy csvvw.py -t $2 | gzip > tmp/min_train.vw.gz
-for i in {18..28}
+for i in {18..27}
 do
+	rm tmp/hash.csv
         zcat tmp/min_train.vw.gz | vw -b $i --invert_hash tmp/hash.csv --initial_weight 100 2>/dev/null;
         echo $i $(tail -n+12 tmp/hash.csv | wc -l) $(tail -n+12 tmp/hash.csv | awk -F: '{print $2}' | sort | uniq | wc -l) >> $3
 done
-for i in {29..40}
+for i in {28..40}
 do
+	rm tmp/hash.csv
 	zcat tmp/min_train.vw.gz | vw -b $i --sparse_weights --invert_hash tmp/hash.csv --initial_weight 100 2>/dev/null;
 	echo $i $(tail -n+12 tmp/hash.csv | wc -l) $(tail -n+12 tmp/hash.csv | awk -F: '{print $2}' | sort | uniq | wc -l) >> $3
 done
